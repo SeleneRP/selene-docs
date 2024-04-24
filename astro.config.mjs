f9849c2112668db81eb2ce56e0a125567f165c24
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-04-24 10:26:03 +0200

    add download link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,60 +3,66 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://selene.twelveiterations.com',
-    integrations: [
-        starlight({
-            title: 'Selene Development',
-            logo: {
-                src: './src/assets/selene.svg',
-            },
-            favicon: '/favicon.svg',
-            social: {
-                github: 'https://github.com/SeleneRP/selene-docs',
-                discord: 'https://discord.gg/S7maQVRRa9',
-                twitter: 'https://twitter.com/BlayTheNinth',
-                twitch: 'https://twitch.tv/BlayTheNinth',
-                youtube: 'https://www.youtube.com/@BlayTheNinth',
-            },
-            sidebar: [
-                {
-                    label: 'Guides',
-                    autogenerate: { directory: '/guides/' },
-                },
-                {
-                    label: 'Reference',
-                    items: [
-                        { label: 'Bundles', autogenerate: { directory: '/reference/bundles/' } },
-                        { label: 'Scripting', 
-						items: [
-							{
-								label: 'Scripting Overview',
-								link: '/reference/scripting/',
-							},
-							{
-								label: 'Resources',
-								collapsed: true,
-								autogenerate: { directory: '/reference/scripting/resource-management' }
-							},
-							{
-								label: 'Maps',
-								collapsed: true,
-								autogenerate: { directory: '/reference/scripting/map' }
-							},
-							{
-								label: 'Camera',
-								collapsed: true,
-								autogenerate: { directory: '/reference/scripting/camera' }
-							}
-						] },
-                        { label: 'Configuration', autogenerate: { directory: '/reference/configuration/' } },
-                    ],
-                },
-            ],
-            editLink: {
-                baseUrl: 'https://github.com/SeleneRP/selene-docs/edit/main/',
-            },
-            lastUpdated: true,
-        }),
-    ],
+  site: 'https://selene.twelveiterations.com',
+  integrations: [starlight({
+    title: 'Selene Development',
+    logo: {
+      src: './src/assets/selene.svg'
+    },
+    favicon: '/favicon.svg',
+    social: {
+      github: 'https://github.com/SeleneRP/selene-docs',
+      discord: 'https://discord.gg/S7maQVRRa9',
+      twitter: 'https://twitter.com/BlayTheNinth',
+      twitch: 'https://twitch.tv/BlayTheNinth',
+      youtube: 'https://www.youtube.com/@BlayTheNinth'
+    },
+    sidebar: [{
+      label: 'Guides',
+      autogenerate: {
+        directory: '/guides/'
+      }
+    }, {
+      label: 'Reference',
+      items: [{
+        label: 'Bundles',
+        autogenerate: {
+          directory: '/reference/bundles/'
+        }
+      }, {
+        label: 'Scripting',
+        items: [{
+          label: 'Scripting Overview',
+          link: '/reference/scripting/'
+        }, {
+          label: 'Resources',
+          collapsed: true,
+          autogenerate: {
+            directory: '/reference/scripting/resource-management'
+          }
+        }, {
+          label: 'Maps',
+          collapsed: true,
+          autogenerate: {
+            directory: '/reference/scripting/map'
+          }
+        }, {
+          label: 'Camera',
+          collapsed: true,
+          autogenerate: {
+            directory: '/reference/scripting/camera'
+          }
+        }]
+      }, {
+        label: 'Configuration',
+        autogenerate: {
+          directory: '/reference/configuration/'
+        }
+      }]
+    }],
+    editLink: {
+      baseUrl: 'https://github.com/SeleneRP/selene-docs/edit/main/'
+    },
+    lastUpdated: true
+  })]
 });
--- src/content/docs/downloads.md
+++ src/content/docs/downloads.md
@@ -2,5 +2,23 @@
 title: Downloads
 ---
 
-Selene is not yet available for download. Please check back later.
+### Latest Release
 
+Selene 0.1.0 is now available, marking the first release of our server software.
+
+This release is intended for initial experimentation with custom bundle development. At this stage, it can not yet be used to reasonably host a game world.
+
+:::tip[Experimental]
+This is a highly experimental version. Many features are not yet available or not yet sufficiently documented.
+:::
+
+#### Download
+- [selene-server.zip](https://downloads.selene.world/selene-server.zip)
+
+-----
+
+### Release Notes
+
+#### 0.1.0
+
+- Initial Release
